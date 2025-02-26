a8cba823add447cef99e0187943d8541d5724825
bcd-kushal <dev@kushalkumarsaha.com>
2025-02-26 11:44:50 +0530

    1749. max absolute subarray sum

--- medium/1524-total-subarrays-with-odd-sum/sol.cpp
+++ medium/1524-total-subarrays-with-odd-sum/sol.cpp
@@ -1,23 +1,26 @@
 #include <vector>
 using namespace std;
 
+#define MOD 1000'000'007
+
 class Solution {
 public:
   int numOfSubarrays(vector<int>& arr) {
-    const int MOD = 1000'000'007;
     int total = 0, s = 0, n = arr.size();
-    vector<vector<int>> pre(3,vector<int>(n,0));
+    vector<vector<int>> pre(2,vector<int>(n,0));
+    bool isOne;
 
     for(int i=0;i<n;i+=1)
       pre[0][i] = ((i==0 ? 0 : pre[0][i-1]) + arr[i]) % 2;
 
     for(int i=n-1;i>=0;i-=1) {
-      pre[1][i] = (i==n-1 ? 0 : pre[1][i+1]) + (pre[0][i]==1 ? 1 : 0);
-      pre[2][i] = (i==n-1 ? 0 : pre[2][i+1]) + (pre[0][i]==0 ? 1 : 0);
+      isOne = pre[0][i] == 1;
+      pre[0][i] = (i==n-1 ? 0 : pre[0][i+1]) + (isOne ? 1 : 0);
+      pre[1][i] = (i==n-1 ? 0 : pre[1][i+1]) + (!isOne ? 1 : 0);
     }
 
     for(int i=0;i<n;i+=1) {
-      total = (total + pre[s+1][i]) % MOD;
+      total = (total + pre[s][i]) % MOD;
       s ^= arr[i] % 2;
     }
 
