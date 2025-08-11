308ebaddb973b1e926d455cd68e6021b74d31655
bcd-kushal <dev@kushalkumarsaha.com>
2025-08-11 14:27:46 +0530

    9. palindromic number

---
+++ easy/0009-palindrome-number/sol.py
@@ -0,0 +1,15 @@
+import math
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+            
+        n = x
+        rev = 0
+
+        while n > 0:
+            rev = rev*10 + n%10
+            n = math.floor(n/10)
+        
+        return rev == x
