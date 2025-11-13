bcf7e1c1390ed0f40435d67c4c80f68037e154f5
sde-kushal <dev@kushalkumarsaha.com>
2025-11-13 05:59:11 +0530

    3228. max moves to shift ones to right

---
+++ medium/3228-max-moves-shift-one-to-right/sol.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        ans = 0
+        ones = 0
+
+        for i in range(len(s)):
+            if s[i] == '1':
+                ones += 1
+            elif i>0 and s[i-1] == '1':
+                ans += ones
+
+        return ans
